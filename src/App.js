36526a88f5e899d3a57f7508385f92a2239eb58b
unknown <24je0716@iitism.ac.in>
2026-06-13 21:48:25 +0530

    Project setup and folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>CodeTrack 🚀</h1>
+      <p>Dashboard loading soon...</p>
+    </div>
+  );
+}
+
+export default App;
