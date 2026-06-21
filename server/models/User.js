116eec8f28e686f54c543d5e020816904b06e3a4
unknown <24je0716@iitism.ac.in>
2026-06-21 12:23:16 +0530

    Fix: Remove wrong testing library import from User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,30 @@
-const { type } = require('@testing-library/user-event/dist/type');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6
-    },
-    lcUsername: {
-        type: String,
-        default: ''
-    },
-    cfUsername: {
-        type: String,
-        default: ''
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  lcUsername: {
+    type: String,
+    default: ''
+  },
+  cfUsername: {
+    type: String,
+    default: ''
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
